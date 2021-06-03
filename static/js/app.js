ba24c3809e8a713f5f23d5e2cf4972a8473c060c
lucille bluth <melissa.mongrella@northwestern.edu>
2021-06-03 18:20:12 -0500

    start of panel data

--- static/js/app.js
+++ static/js/app.js
@@ -1,10 +1,32 @@
+// Build metadata side panel
+function buildPanelData(sample) {
+    d3.json("Data/samples.json").then((data) => {
+      var metadata = data.metadata;
+      console.log(metadata);
+
+    // Filter metadata
+    var buildingArray = metadata.filter(sampleObj => sampleObj.id == sample);
+    var result = buildingArray[0];
+    // Use d3.select to grab value
+    var panelData = d3.select("#sample-metadata");
+
+    // Clear the existing data in the html
+    panelData.html("");
+
+    // Use Object.entries to get key/value pair to the panelData
+    Object.entries(result).forEach(([key, value]) => {
+      panelData.append("h5").text(`${key.toUpperCase()}: ${value}`);
+    });
+  });
+}
+
 
 
   function init() {
-    // Create function for event change on the dropdown
+    // Create the function for event change on the dropdown
     var selectDropdown = d3.selectAll("#selDataset");
   
-    // Populate the select options with the list of sample names
+    // Populate the select options with sample names
     d3.json("Data/samples.json").then((data) => {
       var name = data.names;
   
@@ -14,7 +36,8 @@
           .text(sample)
           .property("value", sample);
       })
- 
+  
+
   
 // Initialize the dashboard
   init()
